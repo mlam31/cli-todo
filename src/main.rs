c5e62ac51c88b40d5b3125b2fed1be21a840a923
mlam31 <mathieu.lam31@gmail.com>
2025-09-24 17:28:50 +0200

    struct and methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::json;
+
+struct Todo {
+    id: u32,
+    title: String,
+    done: bool,
+}
+
+impl Todo {
+    fn add(){
+        let mut ctr_id = 0;
+        Self { id: (ctr_id), title: String::new(), done: false};
+        ctr_id += 1;
+    }
+
+    fn list(){
+        // not implemented
+    }
+
+    fn done(&self, id: u32){
+        // not implemented
+    }
+
+    fn remove(&self, id: u32){
+        // not implemented
+    }
+}
+
+fn main(){
+    println!("Hello World!")
 }
